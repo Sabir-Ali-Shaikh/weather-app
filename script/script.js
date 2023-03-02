9f1beea9414f1bf49831cca0bf2e170511f841ce
Sabir-itobuz <sabir@itobuz.com>
2023-03-02 14:56:03 +0530

    third Version

--- script/script.js
+++ script/script.js
@@ -1,16 +1,15 @@
 let response;
-async function getWeather() {
-  response = await fetch(
-    "http://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=san+francisco&aqi=no"
-  ).then((data) => {
-    console.log(data);
+async function getWeather(locationInput) {
+    response = await fetch(
+        `http://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=${locationInput}&aqi=no`
+    ).then((data) => {
+        console.log(data);
 
-    return data.json();
-  });
-  console.log(response);
-  weatherInfo();
+        return data.json();
+    });
+    console.log(response);
+    weatherInfo();
 }
-getWeather();
 
 let locName = document.querySelector(".city");
 let currentTemp = document.querySelector(".temp");
@@ -23,15 +22,28 @@ let icon = document.querySelector(".icon img");
 let path;
 
 function weatherInfo() {
-  locName.innerHTML = ` ${response.location.name}`;
-  country.innerHTML = `Country:  ${response.location.country}`;
-  lon.innerHTML = `longitude: ${response.location.lon}`;
-  latitude.innerHTML = `longitude: ${response.location.lat}`;
-  text.innerHTML = `Type: ${response.current.condition.text}`;
-  feel.innerHTML = `Feels Like ${response.current.feelslike_c}`;
-  currentTemp.innerHTML = `${response.current.temp_c} <sup>o</sup>`;
-  // path = response.current.condition.icon.replace('//cdn.weatherapi.com', '../assets')
-  icon.src = response.current.condition.icon;
+    locName.value = ` ${response.location.name}`;
+    country.innerHTML = `Country:  ${response.location.country}`;
+    lon.innerHTML = `longitude: ${response.location.lon}`;
+    latitude.innerHTML = `longitude: ${response.location.lat}`;
+    text.innerHTML = `Type: ${response.current.condition.text}`;
+    feel.innerHTML = `Feels Like ${response.current.feelslike_c}<sup>°</sup>`;
+    currentTemp.innerHTML = `${response.current.temp_c} <sup>°</sup>`;
+    path = response.current.condition.icon.replace('//cdn.weatherapi.com', '../assets')
+    path = path.replace('.png','.svg')
+    // icon.src = response.current.condition.icon;
+    icon.src=path
 
-  console.log(icon);
+    console.log(icon);
+}
+
+locName.addEventListener('keyup',(e)=>{
+    if(e.key=="Enter"){
+        getWeather(e.target.value)
+
+    }
+})
+
+window.onload = () => {
+    getWeather("San Francisco");
 }
